6b7340e4e381c596ab3a729a076ff9a3d35a221d
Amit Kumar Gupta <amitkgupta.yugasa@gmail.com>
2018-11-22 10:45:05 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server listening on port ${PORT}`)
+});

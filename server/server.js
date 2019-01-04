27c1ad9962dde42d8414b9f43b8a5e76d9105ee6
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-04 12:44:26 +0530

    nc

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from:'admin',
+        text:'Welcome to chat app'
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from:'admin',
+        text:'New User Joined the chat',
+        createdAt:new Date().getTime()
+    })
+
     socket.on('createMessage',function(msg){
         console.log('createMessage',msg)
         io.emit('newMessage', {
@@ -21,6 +32,12 @@ io.on('connection', (socket)=>{
             text:msg.text,
             createdAt:new Date().getTime()
         })
+
+        // socket.broadcast.emit('newMessage',{
+        //     from:msg.from,
+        //     text:msg.text,
+        //     createdAt:new Date().getTime()
+        // })
     })
 
     socket.on('disconnect',()=>{

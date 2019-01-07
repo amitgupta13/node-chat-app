48550bc59f289d6816875b3a9f9849e2c6c02921
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-07 15:29:24 +0530

    nc

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,26 @@ socket.on('connect', function(){
 
 socket.on('newMessage',function(msg){
     console.log('newMessage', msg);
+    var li = jQuery('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`)
+
+    jQuery('#messages').append(li);
+})
+
+socket.emit('createMessage', {
+    from:'frank',
+    text:'styles'
+}, function(data){
+    console.log('Got It', data);
+})
+
+jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text:jQuery('[name=message]').val()
+    }, function(){
+
+    })
 })
--- server/server.js
+++ server/server.js
@@ -18,9 +18,10 @@ io.on('connection', (socket)=>{
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'))
 
-    socket.on('createMessage',function(msg){
+    socket.on('createMessage',function(msg, callback){
         console.log('createMessage',msg)
         io.emit('newMessage', generateMessage(msg.from, msg.text));
+        callback('This is from the server');
 
         // socket.broadcast.emit('newMessage',{
         //     from:msg.from,

21d85dcedeb1536ca9d216706f3a4ee0c520bfa8
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-08 17:52:19 +0530

    nc

--- server/utils/users.js
+++ server/utils/users.js
@@ -26,6 +26,26 @@ class Users {
         this.users.push(user);
         return user;
     }
+
+    getUserList(room){
+        let user = this.users.filter((user)=> user.room === room);
+        let namesArray = user.map((user)=> user.name);
+
+        return namesArray;
+    }
+
+    getUser(id){
+        return this.users.filter((user)=>user.id === id)[0];
+    }
+
+    removeUser(id){
+        let user = this.getUser(id);
+        if(user){
+            this.users = this.users.filter((user)=>user.id !== id);
+        }
+
+        return user;
+    }
 }
 
 module.exports = {
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,28 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users',()=>{
+
+    let users;
+
+    beforeEach(()=>{
+        users = new Users();
+        users.users = [
+            {
+                id:'1',
+                name:'Mike',
+                room:'Node Course'
+            }, {
+                id:'2',
+                name:'Jen',
+                room:'React Course'
+            }, {
+                id:'3',
+                name:'Julie',
+                room:'Node Course'
+            },
+        ]
+    })
+
     it('it should add new User', ()=>{
         const users = new Users();
 
@@ -11,8 +33,49 @@ describe('Users',()=>{
             room:'A'
         }
     
-        const res = users.addUser(user.id, user.name, user.room);
-    
+        users.addUser(user.id, user.name, user.room);
         expect(users.users).toEqual([user]);
+    })
+
+    it('should return names for node course', ()=>{
+        const userList = users.getUserList('Node Course');
+        
+        expect(userList).toEqual(['Mike', 'Julie']);
+    });
+
+    it('should return names for React course', ()=>{
+        const userList = users.getUserList('React Course');
+        
+        expect(userList).toEqual(['Jen']);
+    });
+
+    it('should find a user', ()=>{
+        var userId = '2'
+        const user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    })
+
+    it('should not find a user', ()=>{
+        var userId = '4'
+        const user = users.getUser(userId);
+
+        expect(user).toNotExist();
+    })
+
+    it('should remove a user', ()=>{
+        var userId = '2'
+        const user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    })
+
+    it('should not remove a user', ()=>{
+        var userId = '4'
+        const user = users.removeUser(userId);
+
+        expect(user).toNotExist();
+        expect(users.users.length).toBe(3);
     })
 });

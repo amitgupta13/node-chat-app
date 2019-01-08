7bc45800aa20563b4808ebadd3d89a984ad48165
Amit Gupta <vikas.s@daffodilsw.com>
2019-01-08 14:53:57 +0530

    nc

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,22 @@
 
 const socket = io();
 
+function scrollToBottom(){
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight>= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function(){
     console.log('connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage',function(msg){
         createdAt:formattedTime
     });
         jQuery('#messages').append(html);
+        scrollToBottom();
     // var li = jQuery('<li></li>');
     // li.text(`${msg.from} ${formattedTime}: ${msg.text}`)
 
@@ -40,6 +57,7 @@ socket.on('newLocationMessage', function(msg){
     // jQuery('#messages').append(li);
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function(e){
